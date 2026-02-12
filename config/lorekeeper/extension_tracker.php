383cbe944c240f5f3262f7b81c6bb867d78e27cf
cedarcall <cedarcall530@gmail.com>
2026-02-11 19:07:58 -0600

    Fix extension tracker for safety deposit box

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -66,5 +66,12 @@ return [
     ]),
     'version' => '2.0.0',
 ],
-
+'safety_deposit_box' => [
+    'key' => 'safety_deposit_box',
+    'wiki_key' => 'Safety_Deposit_Box',
+    'creators' => json_encode([
+        'Uri' => 'https://github.com/preimpression/',
+    ]),
+    'version' => '1.0.0',
+],
 ];
