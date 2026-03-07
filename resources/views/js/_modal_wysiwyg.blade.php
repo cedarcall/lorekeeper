4f1c1ef85f9e2335b190a26e978467d8b69633f0
cedarcall <cedarcall530@gmail.com>
2026-03-07 16:48:02 -0600

    css fixing

--- resources/views/js/_modal_wysiwyg.blade.php
+++ resources/views/js/_modal_wysiwyg.blade.php
@@ -13,9 +13,9 @@ content_css: [
 '{{ asset('css/app.css') }}',
 '{{ asset('css/lorekeeper.css') }}',
 '{{ asset('css/custom.css') }}',
-'{{ asset($theme?->cssUrl) }}',
-'{{ asset($conditionalTheme?->cssUrl) }}',
-'{{ asset($decoratorTheme?->cssUrl) }}',
+@if($theme?->cssUrl) '{{ asset($theme->cssUrl) }}', @endif
+@if($conditionalTheme?->cssUrl) '{{ asset($conditionalTheme->cssUrl) }}', @endif
+@if($decoratorTheme?->cssUrl) '{{ asset($decoratorTheme->cssUrl) }}', @endif
 '{{ asset('css/all.min.css') }}' //fontawesome
 ],
 content_style: `
